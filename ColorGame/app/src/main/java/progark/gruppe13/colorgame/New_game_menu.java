77a21a82adbcd15a541951a03df4f60717ebe062
mhaug91 <mhaug91@gmail.com>
2016-04-14 14:28:25 +0200

    new game menu

--- ColorGame/app/src/main/java/progark/gruppe13/colorgame/New_game_menu.java
+++ ColorGame/app/src/main/java/progark/gruppe13/colorgame/New_game_menu.java
@@ -1,6 +1,7 @@
 package progark.gruppe13.colorgame;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,7 @@ import android.view.ViewGroup;
 /**
  * Created by mac on 14.04.2016.
  */
-public class Martin_menu extends GameState {
+public class New_game_menu extends GameState {
     // TODO: Rename and change types and number of parameters
     public static Start_menu newInstance() {
         Start_menu fragment = new Start_menu();
@@ -18,7 +19,7 @@ public class Martin_menu extends GameState {
         return fragment;
     }
 
-    public Martin_menu() {
+    public New_game_menu() {
         // Required empty public constructor
     }
 
@@ -32,7 +33,11 @@ public class Martin_menu extends GameState {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_martin_menu, container, false);
+        return inflater.inflate(R.layout.fragment_new_game_menu, container, false);
+    }
+
+    public void onNewGameClick(View v){
+        Log.i("klkikk", "klikk");
     }
 
 
