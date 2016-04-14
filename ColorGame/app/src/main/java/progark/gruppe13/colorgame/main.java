6db1020d851b59eea1c18132a31985d26d150cbe
mhaug91 <mhaug91@gmail.com>
2016-04-14 15:32:38 +0200

    kamera
    litt copy pasted

--- ColorGame/app/src/main/java/progark/gruppe13/colorgame/main.java
+++ ColorGame/app/src/main/java/progark/gruppe13/colorgame/main.java
@@ -5,6 +5,7 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -56,6 +57,18 @@ public class main extends Activity implements GameState.OnFragmentInteractionLis
             // no camera on this device
             return false;
         }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 
     public void onNewClick(View v){
