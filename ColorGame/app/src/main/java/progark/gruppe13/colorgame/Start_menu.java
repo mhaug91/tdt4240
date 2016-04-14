1dbba3db0bbdb619ae9288cc0b528673c31a24cd
samoth1601 <samoth1601@gmail.com>
2016-04-14 12:27:52 +0200

    yolo

--- ColorGame/app/src/main/java/progark/gruppe13/colorgame/Start_menu.java
+++ ColorGame/app/src/main/java/progark/gruppe13/colorgame/Start_menu.java
@@ -1,13 +1,11 @@
 package progark.gruppe13.colorgame;
 
-import android.app.Activity;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import progark.gruppe13.colorgame.R;
 
 
 /**
@@ -20,13 +18,7 @@ import progark.gruppe13.colorgame.R;
  */
 public class Start_menu extends GameState {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     *
-     * @return A new instance of fragment start_menu.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static Start_menu newInstance() {
         Start_menu fragment = new Start_menu();
