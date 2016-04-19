81d30a9e5faec455b49582f71603364a36a9c0cb
Ingrid <ingrid.myrann@gmail.com>
2016-04-19 10:48:18 +0200

    roundSummary scoreboard-ish

--- ColorGame/app/src/main/java/progark/gruppe13/colorgame/RoundSummary.java
+++ ColorGame/app/src/main/java/progark/gruppe13/colorgame/RoundSummary.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,15 +33,39 @@ public class RoundSummary extends GameState{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_roundsummary, container, false);
+        View view = inflater.inflate(R.layout.fragment_roundsummary, container, false);
+
+        ArrayList<String> testList = new ArrayList<String>();
+        testList.add("en");
+        testList.add("to");
+        testList.add("tre");
+
+        TextView newtext = (TextView) view.findViewById(R.id.scoreList);
+        newtext.setSingleLine(false);
+        for (String id : testList){
+            newtext.append(id);
+            newtext.append("\n");
+        }
+
+
+        return view;
     }
 
+
+    public void setTopFiveHighScoresList(ArrayList<String> topFiveHighScoresList) {
+        this.topFiveHighScoresList = topFiveHighScoresList;
+    }
+
+
+
     public void showResults(){
+
 
     }
 
     @Override
     public void update() {
+        showResults();
     }
     @Override
     public void onEnter() {
